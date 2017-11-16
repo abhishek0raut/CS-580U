321e72e1ecda39cfd27801127f3800ef71f96af1
Abhishek Dinkar Raut <9390583+abhishek0raut@users.noreply.github.com>
2017-11-15 23:16:54 -0500

    Add files via upload

---
+++ Recursion.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int countToZero(int num)
+{
+    if(num > 0)
+    {
+        printf("%d \n", num);
+        num--;
+        countToZero(num);
+    }
+    else if(num < 0)
+    {
+        printf("%d \n", num);
+        num++;
+        countToZero(num);
+
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+int main()
+{
+    int num;
+    printf("Enter a positive or negative number to count to zero from:");
+    scanf("%d",&num);
+    printf("Counting given Number to zero: \n");
+    num = countToZero(num);
+    printf("%d",num);
+    return 0;
+}
